24dd439bc68b7772e48529680bfdf674afe223e8
Andrés Gabriel Fernández Romero <andresgabriel309@gmail.com>
2026-05-24 00:10:40 -0500

    middlewares para control de acceso

--- micro-habitaciones/src/middlewares/checkIfAdmin.js
+++ micro-habitaciones/src/middlewares/checkIfAdmin.js
@@ -1,5 +1,24 @@
-async function checkIfAdmin(req, res, next){
+async function checkIfAdmin(req, res, next) {
+  try {
+    const { user } = req
 
+    if (user.rol !== 'admin') {
+      res.status(404).json({
+        success: false,
+        message: 'El usuario no tiene permisos para realizar esta acción',
+      })
+      return
+    }
+
+    next()
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'Error al procesar el usuario',
+      error: error instanceof Error ? error.message : 'Error desconocido',
+    })
+    return
+  }
 }
 
-export {checkIfAdmin}
+export { checkIfAdmin }
--- micro-habitaciones/src/middlewares/checkValidToken.js
+++ micro-habitaciones/src/middlewares/checkValidToken.js
@@ -31,11 +31,10 @@ async function checkValidToken(req, res, next) {
 
     // Añadir los datos del usuario a la petición para ser usados por el controller
     req.user = response.data
-    
+
     // Mandar la petición al siguiente paso
     next()
-  }
-  catch (error) {
+  } catch (error) {
     res.status(500).json({
       success: false,
       message: 'Error al obtener usuario',
